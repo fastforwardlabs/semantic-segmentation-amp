bf6fed3a63085412ebea8e71a1bafe30c76caf2b
Andrew Reed <areed@cloudera.com>
2022-09-14 20:14:58 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="segmentation-defect-detection",
+    version="0.1.0",
+    description="A demo of semantic segmentation for defect detection.",
+    author="Andrew Reed",
+    author_email="andrew.reed.r@gmail.com",
+    packages=find_packages(),
+)
